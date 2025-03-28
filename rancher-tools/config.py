0634f73bfcf8f0608d5935d759c1ddd549b34550
kcalls <karthikmynickname@gmail.com>
2025-03-28 13:53:39 -0500

    Create config.py

---
+++ rancher-tools/config.py
@@ -0,0 +1,14 @@
+RANCHER_ENVIRONMENTS = {
+    'test': {
+        'url': 'https://rancher-test.example.com',
+        'description': 'Test environment'
+    },
+    'uat': {
+        'url': 'https://rancher-uat.example.com',
+        'description': 'UAT environment'
+    },
+    'prod': {
+        'url': 'https://rancher-prod.example.com',
+        'description': 'Production environment'
+    }
+}
